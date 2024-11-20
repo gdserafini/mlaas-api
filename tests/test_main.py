7c5de79a0d5661ff15a1a26147e1c48c5a2442cc
gdserafini <gdinizserafini@gmail.com>
2024-11-20 17:45:20 -0300

    first commit

---
+++ tests/test_main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from main import app
+from http import HTTPStatus
+
+
+client = TestClient(app)
+
+
+def test_root_should_return_ok():
+    response = client.get("/")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"Message": "Hello World"}
